9ee433a6c31db0cb40e7b2acfc86f195ba897e1c
Alex Stiff <alexstiff91@gmail.com>
2025-01-03 12:39:52 +0000

    add more weeks

--- update_files.py
+++ update_files.py
@@ -26,57 +26,40 @@ class CheckoutDartsEnum(str, Enum):
 
 
 class Game(BaseModel):
-    score: list[int | CheckoutDartsEnum] = []
+    score: list[int] = []
     total_score: int = 0
     avg: int = 0
     darts: int = 0
     winner: bool = False
     player: str = ""
     
-    def set_darts_and_avg(self):
-        for s in self.score:
-            if type(s) == int:
-                self.darts += 3
-                self.total_score += s
-            else:
-                self.darts += int(s.replace('x', ''))
-                self.winner = True
-                self.total_score = 501
-                
+    def set_stats(self):
+        self.total_score = sum(self.score)
+        self.winner = self.total_score == 501
         self.avg = self.total_score / self.darts * 3
 
     def print_game(self):
-        score = 501
+        remaining = 501
 
-        remaining = [501]
-
-        for throw_score in self.score:
-            try:
-                score -= throw_score
-            except TypeError:
-                score -= score
-
-            remaining.append(score)
-
-
-        print(f"| {self.home_player: <7} |     |")
-        print("| ------- | --- |")
-        print("|         | 501 |")
+        print(f"| {self.player: <7} |     |")
+        print("| ------- | --- | --- |")
+        print("|         | 501 |     |")
 
         game_over = False
 
-        for i in range(100):
-            try:
-                home = self.home_score[i]
-            except IndexError:
-                home = ''
-            
-            try:
-                home_left = remaining[i+1]
-            except IndexError:
-                game_over = True
+        # in 49 darts 49 % 3 == 1 (x1)
+        # in 50 darts 50 % 3 == 2 (x2)
+        # in 51 darts 51 % 3 == 0 (x3)
 
-            print(f"| {home: <7} | {home_left: <3} |")
+        for i, score in enumerate(self.score):
+            if score == remaining:
+                score = f"x{(self.darts-1) % 3 + 1}"
+                game_over = True
+                remaining = 0
+            else:
+                remaining -= score
+
+            print(f"| {score: <7} | {remaining: <3} | {(i+1) * 3: <3} |")
 
             if game_over:
                 print()
@@ -89,8 +72,8 @@ class Match(BaseModel):
     games: list[Game] = []
     avg: float = 0
 
-    def add_game(self, score: list[str]):
-        game = Game(home_score=score, player=self.player)
+    def add_game(self, score: list[int]):
+        game = Game(score=score)
         self.games.append(game)
 
         if game.winner:
@@ -109,13 +92,25 @@ def get_match_from_file(filename: str) -> Match:
     with open(filename) as f:      
         for game in f.readlines():
             score = game.rstrip().split(' ')
+
+            game_score: list[int] = []
+            darts: int = 0
+
+            for s in score:
+                try:
+                    game_score.append(int(s))
+                    darts += 3
+                except Exception:
+                    game_score.append(501 - sum(game_score))
+                    darts += int(s.replace('x', ''))
             
             game = Game(
-                score=score,
+                score=game_score,
+                darts=darts,
                 player=player.capitalize(),
             )
 
-            game.set_darts_and_avg()
+            game.set_stats()
 
             match.games.append(game)
     
@@ -161,16 +156,17 @@ for week in glob.glob('_python_source/*'):
 
         for filename in glob.glob(f'{week}/*.csv'):
             match = get_match_from_file(filename)
+    
             weekly_avg = sum([game.total_score for game in match.games])/sum([game.darts for game in match.games]) * 3
             f.write(f"{match.player},{weekly_avg:.2f}\n")
 
             players[match.player].games += match.games
 
-for name, player in players.items():
-    print(name)
-    for game in player.games:
-        print(game.avg, game.darts, game.score)
-    print()
+# for name, player in players.items():
+#     print(name)
+#     for game in player.games:
+#         print(game.avg, game.darts, game.score)
+#     print()
 
 with open('_data/season.csv', 'w') as f:
     f.write("name,games,average\n")
